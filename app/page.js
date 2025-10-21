b720c8dacb5f1981793d413a9263bf01c6ccbabb
Mominul Haque Sagar <mominulhaquesagar@gmail.com>
2025-10-22 00:34:33 +0600

    Added scroll down animation

--- app/page.js
+++ app/page.js
@@ -7,20 +7,25 @@ import PrayerTimes from "./components/PrayerTimes";
 import AboutMosque from "./components/AboutMosque";
 import ServiceProgram from "./components/ServiceProgram";
 import VisitUs from "./components/VisitUs";
+import CustomScrollDown from "@/components/customScrollDown/CustomScrollDown";
 
 export default function Home() {
   return (
     <Grid className="gap-4 grid-cols-10" >
       <GridItem className="col-span-10">
-        <Center className="pt-[20px] sm:pt-[30px] md:pt-[50px] pb-[15px] sm:pb-[25px] md:pb-[40px]">
-          <Text className="text-center text-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">Welcome to Khanjahan Ali (R:) Jame Masjid</Text>
-        </Center>
+        <CustomScrollDown>
+          <Center className="pt-[20px] sm:pt-[30px] md:pt-[50px] pb-[15px] sm:pb-[25px] md:pb-[40px]">
+            <Text className="text-center text-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">Welcome to Khanjahan Ali (R:) Jame Masjid</Text>
+          </Center>
+        </CustomScrollDown>
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center">
-        <Center className="w-[100%] sm:w-[70%] md:w-[60%] lg:w-[50%]">
-          <Text className="text-center text-black text-lg sm:text-xl">A place of worship, community, and spiritual growth. Join us in prayer, learning, and service to strengthen our faith and community bonds.</Text>
-        </Center>
+        <CustomScrollDown>
+          <Center className="w-[100%] sm:w-[70%] md:w-[60%] lg:w-[50%] m-auto">
+            <Text className="text-center text-black text-lg sm:text-xl">A place of worship, community, and spiritual growth. Join us in prayer, learning, and service to strengthen our faith and community bonds.</Text>
+          </Center>
+        </CustomScrollDown>
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center mt-[40px] md:mt-[70px]">
@@ -30,19 +35,27 @@ export default function Home() {
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center my-[40px] md:my-[70px]">
-        <PrayerTimes />
+        <CustomScrollDown>
+          <PrayerTimes />
+        </CustomScrollDown>
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center my-[40px] md:my-[70px]">
-        <AboutMosque />
+        <CustomScrollDown>
+          <AboutMosque />
+        </CustomScrollDown>
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center my-[40px] md:my-[70px]">
-        <ServiceProgram />
+        <CustomScrollDown>
+          <ServiceProgram />
+        </CustomScrollDown>
       </GridItem>
 
       <GridItem className="col-span-10 flex justify-center my-[40px] md:my-[70px]">
-        <VisitUs />
+        <CustomScrollDown>
+          <VisitUs />
+        </CustomScrollDown>
       </GridItem>
     </Grid >
   )
---
+++ components/customScrollDown/CustomScrollDown.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+export default function CustomScrollDown({ children, delay = 0 }) {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 70 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.8, ease: "easeOut", delay }}
+    >
+      {children}
+    </motion.div>
+  )
+}
